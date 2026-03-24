5f41eed63cd85d48148b24263b11f0ad116615b5
AdrianZ <adrianzygar@wp.pl>
2026-03-24 21:20:01 +0100

    Przejscie na NumPy; wykorzystanie masek do odznaczenia frontierow; odizlowanie map_callback od ciezkich matematycznych zadan i tylko skierowanie jej ku zapisowi mapy; logika przeniesiona do def exploration_loop; dodanie znacznikow dla celu oraz klasterów

--- src/fire_rover_navigation/fire_rover_navigation/goal_sender.py
+++ src/fire_rover_navigation/fire_rover_navigation/goal_sender.py
@@ -1,5 +1,8 @@
 import math
 import rclpy
+import numpy as np
+import scipy.ndimage as ndimage
+from scipy.spatial.transform import Rotation as R
 from rclpy.node import Node
 from rclpy.action import ActionClient
 from geometry_msgs.msg import PoseStamped
@@ -10,7 +13,8 @@ from rclpy.time import Time
 from tf_transformations import quaternion_from_euler
 from time import time
 from collections import deque
-
+from action_msgs.msg import GoalStatus
+from visualization_msgs.msg import Marker, MarkerArray
 
 class GoalSender(Node):
 
@@ -19,34 +23,39 @@ class GoalSender(Node):
 
         # tworzymy klienta action
         self._action_client = ActionClient(self, NavigateToPose, "/navigate_to_pose")
+
         # subskrybujemy mapę
         self.map_subscriber = self.create_subscription(
             OccupancyGrid, "/map", self.map_callback, 10
         )
+
+        self.marker_pub = self.create_publisher(MarkerArray, "/frontiers_markers", 10)
 
         # czekamy na serwer action
         self.get_logger().info("Czekam na serwer action... ")
         self._action_client.wait_for_server()
 
         # inicjalizacja stanu robota
-        self.robot_is_busy = False
+        self.current_goal = None
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
-        self.last_call = 0
+        self.last_call = None
+        self.exploration_timer = self.create_timer(2.0,self.exploration_loop)
 
-    def map_callback(self, msg):
+    def map_callback(self,msg):
+        self.latest_map=msg
 
-        # Ograniczamy częstotliwość wywoływania tej funkcji do 1 na 2 sekundy, żeby nie wysyłać zbyt wielu celów do Nav2
-        if time() - self.last_call < 2:
+    def exploration_loop(self):
+
+
+        if self.latest_map is None:
+            self.get_logger().warn("Brak mapy")
             return
-        self.last_call = time()
 
-        # Sprawdzamy stan robota
-        if self.robot_is_busy:
-            return
 
         # Pobieramy aktualną pozycję robota + TF map -> base_footprint
-        self.map_data = msg
+        msg=self.latest_map
+        self.mapa_data=msg
         width = msg.info.width
         height = msg.info.height
         data = msg.data
@@ -63,159 +72,102 @@ class GoalSender(Node):
         origin_y = msg.info.origin.position.y
         resolution = msg.info.resolution
 
-        # BFS osiągalność
-        rx = int((robot_x - origin_x) / resolution)
-        ry = int((robot_y - origin_y) / resolution)
-        reachable = set()
-        bfs_queue = deque([(rx, ry)])
-        reachable.add((rx, ry))
+        if self.current_goal is not None:
+            dist_to_goal = math.hypot(self.current_goal[0] - robot_x, self.current_goal[1] - robot_y)
 
-        while bfs_queue:
-            cx, cy = bfs_queue.popleft()
+            if dist_to_goal > 1.0:
+                return
 
-            for dx in [-1, 0, 1]:
-                for dy in [-1, 0, 1]:
 
-                    nx = cx + dx
-                    ny = cy + dy
+        grid=np.array(data, dtype=np.int8).reshape((height, width))
 
-                    if nx < 0 or ny < 0 or nx >= width or ny >= height:
-                        continue
+        free_space=(grid == 0)
+        unknown_space=(grid == -1)
+        obstacles=(grid == 100)
 
-                    if (nx, ny) in reachable:
-                        continue
+        unknown_expanded=ndimage.binary_dilation(unknown_space)
+        obstacles_expanded=ndimage.binary_dilation(obstacles, iterations=4)
+        frontiers_mask=free_space & unknown_expanded & ~obstacles_expanded
 
-                    index = ny * width + nx
+        labeled_frontiers, num_clusters = ndimage.label(frontiers_mask)
 
-                    if data[index] != 0:
-                        continue
-
-                    reachable.add((nx, ny))
-                    bfs_queue.append((nx, ny))
-
-                # Algorytm wykrywania frontierów
-
-        frontiers = []
-
-        for y in range(height):
-            for x in range(width):
-                index = y * width + x
-                cell = data[index]
-                if cell == 0:  # sprawdzamy tylko wolne komórki
-                    # Sprawdzamy, czy dana wolna komórka jest frontierem tzn. sąsiąd ma wartość -1
-                    if self.has_unknown_neighbor(
-                        x, y, data, width, height
-                    ) and self.is_safe_cell(x, y, data, width, height):
-                        frontiers.append((x, y))
-
-        if not frontiers:
-            self.get_logger().info("Eksploracja zakonczona!")
+        if num_clusters == 0:
+            self.get_logger().info("Brak frontiera")
             return
 
-        # Grupujemy frontier w klastry (sąsiadujące ze sobą frontiers traktujemy jako jeden cel)
-        visited = set()
-        frontier_set = set(frontiers)
-        clusters = []
+        cluster_sizes=ndimage.sum(frontiers_mask, labeled_frontiers, range(1,num_clusters+1))
 
-        for point in frontiers:
-            if point in visited:
-                continue
-            cluster = []
-            queue = deque([point])
-            visited.add(point)
+        valid_cluster_ids=[i+1 for i, size in enumerate(cluster_sizes) if size >= 20]
 
-            while queue:
-                current = queue.popleft()
-                cluster.append(current)
-                cx, cy = current
-                for dx in [-1, 0, 1]:
-                    for dy in [-1, 0, 1]:
-                        nx = cx + dx
-                        ny = cy + dy
-                        neighbor = (nx, ny)
+        if not valid_cluster_ids:
+            self.get_logger().info("Brak frontiera")
+            return
 
-                        if neighbor in frontier_set and neighbor not in visited:
-                            visited.add(neighbor)
-                            queue.append(neighbor)
+        centroids=ndimage.center_of_mass(frontiers_mask, labeled_frontiers, valid_cluster_ids)
 
-            if len(cluster) >= 20:
-                clusters.append(cluster)
-
-        self.get_logger().info(f"Liczba clusterów: {len(clusters)}")
-
-        # Dla każdego klastra obliczamy jego centroid i traktujemy go jako potencjalny cel
-        best = None
         best_score = float("-inf")
+        best_world_x = None
+        best_world_y = None
 
-        for cluster in clusters:
-            # Filtrujemy klastry, które nie są osiągalne przez robota
-            cluster_reachable = [p for p in cluster if p in reachable]
-            if not cluster_reachable:
-                continue
+        for idx, (cy, cx) in enumerate(centroids):
+                #Konwersja GRID -> ŚWIAT
+                world_x = origin_x + cx * resolution
+                world_y = origin_y + cy * resolution
 
-            best_point = min(
-                cluster_reachable,
-                key=lambda p: math.hypot(
-                    msg.info.origin.position.x
-                    + (p[0] + 0.5) * msg.info.resolution
-                    - robot_x,
-                    msg.info.origin.position.y
-                    + (p[1] + 0.5) * msg.info.resolution
-                    - robot_y,
-                ),
-            )
+                #Obliczanie odległości od robota
+                dist_to_robot = math.hypot(world_x - robot_x, world_y - robot_y)
 
-            # Konwersja GRID -> ŚWIAT
-            wx = (
-                msg.info.origin.position.x + (best_point[0] + 0.5) * msg.info.resolution
-            )
-            wy = (
-                msg.info.origin.position.y + (best_point[1] + 0.5) * msg.info.resolution
-            )
+                if dist_to_robot < 1.0:
+                    continue
 
-            dist = math.hypot(wx - robot_x, wy - robot_y)
+                c_id = valid_cluster_ids[idx]
+                size = cluster_sizes[c_id-1]
 
-            score = len(cluster_reachable) - dist * 0.5
+                score=size-(dist_to_robot*0.5)
 
-            if dist < 1.0:  # ignorujemy cele zbyt blisko robota
-                continue
+                if score > best_score:
+                    best_score = score
+                    best_world_x = world_x
+                    best_world_y = world_y
 
-            if score > best_score:
-                best_score = score
-                best = (wx, wy)
-
-        if best is None:
-            self.get_logger().info("Nie można znaleźć frontiera!")
+        if best_world_x is None:
+            self.get_logger().info("Frontiery sa zbyt blisko")
             return
 
-        world_x, world_y = best
+        world_x=best_world_x
+        world_y=best_world_y
 
-        # Obliczamy orientacje
+        self.get_logger().info(f"Wybrano frontiera: ({world_x:.2f}, {world_y:.2f})")
+
+        # Publikacja markerów w RViz
+        self.publish_markers(centroids, origin_x, origin_y, resolution, world_x, world_y)
+
+        goal_msg=NavigateToPose.Goal()
+        goal_msg.pose.header.frame_id="map"
+        goal_msg.pose.header.stamp=self.get_clock().now().to_msg()
+        goal_msg.pose.pose.position.x=world_x
+        goal_msg.pose.pose.position.y=world_y
+
         yaw = math.atan2(world_y - robot_y, world_x - robot_x)
-        qx, qy, qz, qw = quaternion_from_euler(0, 0, yaw)
+        q = R.from_euler('z', yaw).as_quat()
+        goal_msg.pose.pose.orientation.x = q[0]
+        goal_msg.pose.pose.orientation.y = q[1]
+        goal_msg.pose.pose.orientation.z = q[2]
+        goal_msg.pose.pose.orientation.w = q[3]
 
-        # Wysyłamy goal do Nav2
-        goal_msg = NavigateToPose.Goal()
-        goal_msg.pose.header.frame_id = "map"
-        goal_msg.pose.header.stamp = self.get_clock().now().to_msg()
-        goal_msg.pose.pose.position.x = world_x
-        goal_msg.pose.pose.position.y = world_y
 
-        goal_msg.pose.pose.orientation.x = qx
-        goal_msg.pose.pose.orientation.y = qy
-        goal_msg.pose.pose.orientation.z = qz
-        goal_msg.pose.pose.orientation.w = qw
 
-        self.robot_is_busy = True
 
-        self.result_future = self._action_client.send_goal_async(goal_msg)
+        self.current_goal=(world_x, world_y)
+        self.result_future=self._action_client.send_goal_async(goal_msg)
         self.result_future.add_done_callback(self.goal_response_callback)
+
 
     def goal_response_callback(self, future):
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().info("Goal odrzucony ❌")
-            self.robot_is_busy = False
+            self.current_goal = None
             return
 
         self.get_logger().info("Goal zaakceptowany ✅")
@@ -224,43 +176,71 @@ class GoalSender(Node):
         self.result_future.add_done_callback(self.get_result_callback)
 
     def get_result_callback(self, future):
-        self.get_logger().info("Dotarłem do celu 🎯")
-        self.robot_is_busy = False
+        status = future.result().status
 
-    def has_unknown_neighbor(self, x, y, data, width, height):
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info("Hura! Dotarłem do celu! 🎯")
 
-        for dx in [-1, 0, 1]:
-            for dy in [-1, 0, 1]:
+        self.current_goal = None
 
-                nx = x + dx
-                ny = y + dy
+    def publish_markers(self, centroids, origin_x, origin_y, resolution, best_x, best_y):
+        marker_array = MarkerArray()
+        
+        # 1. Czyszczenie starych markerów
+        delete_marker = Marker()
+        delete_marker.action = Marker.DELETEALL
+        marker_array.markers.append(delete_marker)
 
-                if nx < 0 or ny < 0 or nx >= width or ny >= height:
-                    continue
-
-                index = ny * width + nx
-                if data[index] == -1:
-                    return True
-
-        return False
-
-    def is_safe_cell(self, x, y, data, width, height):
-        for dx in [-1, 0, 1]:
-            for dy in [-1, 0, 1]:
-
-                nx = x + dx
-                ny = y + dy
-
-                if nx < 0 or ny < 0 or nx >= width or ny >= height:
-                    continue
-
-                index = ny * width + nx
-
-                if data[ny * width + nx] == 100:  # zajęta komórka
-                    return False
-
-        return True
-
+        # 2. Rysowanie wszystkich klastrów
+        for i, (cy, cx) in enumerate(centroids):
+            marker = Marker()
+            marker.header.frame_id = "map"
+            marker.header.stamp = self.get_clock().now().to_msg()
+            marker.ns = "all_frontiers"
+            marker.id = i
+            marker.type = Marker.SPHERE
+            marker.action = Marker.ADD
+            
+            marker.scale.x = 0.2
+            marker.scale.y = 0.2
+            marker.scale.z = 0.2
+            
+            marker.color.r = 1.0
+            marker.color.g = 0.0
+            marker.color.b = 0.0
+            marker.color.a = 0.6
+            
+            marker.pose.position.x = origin_x + cx * resolution
+            marker.pose.position.y = origin_y + cy * resolution
+            marker.pose.position.z = 0.0
+            
+            marker_array.markers.append(marker)
+            
+        # 3. Zwycięzca - zielona sfera
+        best_marker = Marker()
+        best_marker.header.frame_id = "map"
+        best_marker.header.stamp = self.get_clock().now().to_msg()
+        best_marker.ns = "best_frontier"
+        best_marker.id = 9999
+        best_marker.type = Marker.SPHERE
+        best_marker.action = Marker.ADD
+        
+        best_marker.scale.x = 0.5
+        best_marker.scale.y = 0.5
+        best_marker.scale.z = 0.5
+        
+        best_marker.color.r = 0.0
+        best_marker.color.g = 1.0
+        best_marker.color.b = 0.0
+        best_marker.color.a = 0.9
+        
+        best_marker.pose.position.x = best_x
+        best_marker.pose.position.y = best_y
+        best_marker.pose.position.z = 0.1
+        
+        marker_array.markers.append(best_marker)
+        
+        self.marker_pub.publish(marker_array)
 
 def main(args=None):
     rclpy.init(args=args)
